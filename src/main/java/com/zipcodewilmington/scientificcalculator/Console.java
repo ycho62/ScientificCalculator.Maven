6ab87948d603849931018acb21c1bc060a5d4a10
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-02-25 23:07:38 -0500

    I made changes

--- src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -23,10 +23,48 @@ public class Console {
     }
 
     public static Integer getIntegerInput(String prompt) {
-        return null;
-    }
+        Scanner integerScanner = new Scanner(System.in);
+        println(prompt);
+        Integer int1 = integerScanner.nextInt();
+        return int1 ;}
 
     public static Double getDoubleInput(String prompt) {
-        return null;
+        Scanner doubleScanner = new Scanner(System.in);
+        println(prompt);
+        double dub1 = doubleScanner.nextDouble();
+        return dub1;
+    }
+    public static Scanner getOperatorInput(String oper, double num1, double num2) {
+        char operator;
+//        double num1, num2;
+//        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to our Amazing Calculator!");
+//        num1 = input.nextDouble();
+        operator = input.next().charAt(0);
+//        num2 = input.nextDouble();
+
+//        input.close();
+
+        switch(operator) {
+            case '+':
+                System.out.printf("%.2f + %.2f= %.2f", num1,num2,(num1 + num2));
+                break;
+            case '-':
+                System.out.printf("%.2f - %.2f= %.2f", num1,num2,(num1 - num2));
+                break;
+            case '*':
+                System.out.printf("%.2f * %.2f= %.2f", num1,num2,(num1 * num2));
+                break;
+            case '/':
+                if (num2 !=0) {
+                    System.out.printf("%.2f / %.2f= %.2f", num1, num2, (num1 / num2));
+                } else {
+                    System.out.println("Can not divide by 0!");
+                }
+            default:
+                System.out.println("%c is not an operator!");
+                return input;
+        }
+        return input;
     }
 }
--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -6,12 +6,17 @@ package com.zipcodewilmington.scientificcalculator;
 public class MainApplication {
     public static void main(String[] args) {
         Console.println("Welcome to my calculator!");
-        String s = Console.getStringInput("Enter a string");
-        Integer i = Console.getIntegerInput("Enter an integer");
-        Double d = Console.getDoubleInput("Enter a double.");
+//        String s = Console.getStringInput("Enter a string");
+//        Integer i = Console.getIntegerInput("Enter an integer");
+//        Double d = Console.getDoubleInput("Enter a double.");
+//        Console.println("The user input %s as a string", s);
+//        Console.println("The user input %s as a integer", i);
+//        Console.println("The user input %s as a d", d);
+        Double num1 = Console.getDoubleInput("Enter first value");
+        String oper = Console.getStringInput("Enter an operator");
+        Double num2 = Console.getDoubleInput("Enter second value");
+        Double output = Console.getOperatorInput(oper, num1, num2);
 
-        Console.println("The user input %s as a string", s);
-        Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
+
     }
 }
