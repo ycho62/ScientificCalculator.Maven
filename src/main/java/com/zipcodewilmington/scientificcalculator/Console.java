9a762e72918f764d68d774cabc942d4f4d26ccd7
Yun <yun@zipcoders-MacBook-Pro-2.local>
2022-02-27 00:45:20 -0500

    made changes

--- src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -1,7 +1,5 @@
 package com.zipcodewilmington.scientificcalculator;
-
 import java.util.Scanner;
-
 /**
  * Created by leon on 2/9/18.
  */
@@ -13,6 +11,7 @@ public class Console {
 
     public static void println(String output, Object... args) {
         print(output + "\n", args);
+
     }
 
     public static String getStringInput(String prompt) {
@@ -22,11 +21,197 @@ public class Console {
         return userInput;
     }
 
+
     public static Integer getIntegerInput(String prompt) {
-        return null;
+        Scanner intscan = new Scanner(System.in);
+        println(prompt);
+        Integer int1 = intscan.nextInt();
+
+        return int1;
     }
 
     public static Double getDoubleInput(String prompt) {
-        return null;
+        Scanner dubscan = new Scanner(System.in);
+        println(prompt);
+        Double double1 = dubscan.nextDouble();
+
+        return double1;
     }
+
+    public static Double basicmath(String operate, double value1, double value2) {
+        double value3 = 0;
+
+        String error;
+        if (operate.equals("+")) {
+            value3 = value1 + value2;
+        } else if (operate.equals("-")) {
+            value3 = value1 - value2;
+        } else if (operate.equals("*")) {
+            value3 = value1 * value2;
+        } else if (operate.equals("/")) {
+            if (value2 == 0) {
+                System.out.println("Arrrr");
+            } else {
+                value3 = value1 / value2;
+            }
+        } else {
+            System.out.println("That's not an operator!");
+        }
+
+        return value3;
+
+    }
+
+/*    public static String basicmath2(String test){
+        Double valtest1 = Console.getDoubleInput("Enter a value");
+        for (int i = 0, !operator.equals("exit"), i++) {
+            Double val1 = Console.getDoubleInput("Enter a value1");    //value1
+            //String operator = Console.getStringInput("Enter an operator"); //get operator
+            Double val2 = Console.getDoubleInput("Enter a value2.");       //value2
+            Double value3 = Console.basicmath(operator, val1, val2);
+            value1 = value3;
+        }
+        return String.format("%s", 1);
+    }*/
+
+    public static String getStringArray(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        String userInput = scanner.nextLine();
+        return userInput;
+    }
+
+
+    //Yun's code
+    public static double exponents (double value1){
+        System.out.println("options: square, square root, exponentiation, inverse, invert");
+        Scanner in = new Scanner(System.in);
+        String operate1 = in.nextLine();
+       double value3 = 0;
+    switch (operate1) {
+        case "square":
+            value3 = value1 * value1;
+            System.out.println("Square of " + value1 +" = " + value3);
+            break;
+        case "square root":
+            value3 = Math.sqrt(value1);
+            System.out.println("Square root of " + value1 +" = " + value3);
+            break;
+        case "exponentiation":
+            System.out.println("Enter value 2");
+            double value2 = in.nextDouble();
+            value3 = Math.pow(value1, value2);
+            System.out.println("Exponentiation of " + value1 + " to " + value2 + " = " + value3);
+            break;
+        case "inverse":
+            value3 = 1 / value1;
+            System.out.println(value3);
+            break;
+        case "invert":
+            value3 = value1 * -1;
+            System.out.println(value3);
+            break;
+        default:
+            break;
+
+    }
+        return value3;
+    }
+
+    public static double trigFunctions(double value1) {
+
+        double output = 0;
+        Scanner in = new Scanner(System.in);
+
+        //if (operate1.equals("sin") || operate1.equals("cos") || operate1.equals("tan") || operate1.equals("inverse sin") || operate1.equals("inverse cos") || operate1.equals("inverse tan"))
+        //;
+        System.out.println("radians or degrees?");
+        String radiansOrDegrees = in.nextLine();
+
+        System.out.println("Pick a trig - sin cos tan asin acos atan");
+        String operate1 = in.nextLine();
+
+        if (radiansOrDegrees.equals("degrees") || radiansOrDegrees.equals("Degrees")) {
+
+            switch (operate1) {
+                case "sin":
+                    output = Math.sin(Math.toRadians(value1));
+                    System.out.println("degrees sin(" + value1 +") = " + output);
+                    break;
+                case "cos":
+                    if (value1 == 90) {
+                        output = Math.round(Math.cos(Math.toRadians(value1)));
+                        System.out.println("degrees cos(" + value1 +") = " + output);
+                    } else {
+                        output = Math.cos(Math.toRadians(value1));
+                        System.out.println("degrees cos(" + value1 +") = " + output);
+                    }
+                    break;
+                case "tan":
+                    output = Math.tan(Math.toRadians(value1));
+                    System.out.println("degrees tan(" + value1 +") = " + output);
+                    break;
+                case "asin":
+                    output = Math.asin(Math.toRadians(value1));
+                    System.out.println("degrees asin(" + value1 +") = " + output);
+                    break;
+                case "acos" :
+                    output = Math.acos(Math.toRadians(value1));
+                    System.out.println("degrees acos(" + value1 +") = " + output);
+                    break;
+                case "atan":
+                    output = Math.atan(Math.toRadians(value1));
+                    System.out.println("degrees atan(" + value1 +") = " + output);
+                    break;
+                default:
+                    break;
+            }
+
+        } else {
+
+            switch (operate1) {
+                case "sin":
+                    output = Math.sin(value1);
+                    System.out.println("radian sin(" + value1 +") = " + output);
+                    break;
+                case "cos":
+                    output = Math.cos(value1);
+                    System.out.println("radian cos(" + value1 +") = " + output);
+                    break;
+                case "tan":
+                    output = Math.tan(value1);
+                    System.out.println("radian tan(" + value1 +") = " + output);
+                    break;
+                case "asin":
+                    output = Math.asin(value1);
+                    System.out.println("radian asin(" + value1 +") = " + output);
+                    break;
+                case "acos":
+                    output = Math.acos(value1);
+                    System.out.println("radian acos(" + value1 +") = " + output);
+                    break;
+                case "atan":
+                    output = Math.atan(value1);
+                    System.out.println("radian atan(" + value1 +") = " + output);
+                    break;
+                default:
+                    break;
+            }
+        }
+        return output;
+    }
+    public static double logs (double value1){
+        System.out.println("options: log, 1/log, ln, 1/ln");
+        Scanner in = new Scanner(System.in);
+        String operate1 = in.nextLine();
+        double value3 = 0;
+        switch (operate1) {
+            case "log":
+                value3 = Math.log(value1);
+                System.out.println("log of " + value1 +" = " + value3);
+                break;
+            case "1/log":
+                value3 = Math.(value1);
+                System.out.println("log of " + value1 +" = " + value3);
+                break;
 }
--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -4,14 +4,69 @@ package com.zipcodewilmington.scientificcalculator;
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
+
     public static void main(String[] args) {
         Console.println("Welcome to my calculator!");
-        String s = Console.getStringInput("Enter a string");
-        Integer i = Console.getIntegerInput("Enter an integer");
-        Double d = Console.getDoubleInput("Enter a double.");
+        //String s = Console.getStringInput("Enter a string");
 
-        Console.println("The user input %s as a string", s);
-        Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
+        String[] operate        = new String[100];
+        String[] operatetrig    = new String[100];
+        double[] memory         = new double[100];
+
+
+        int op = 0;
+        double initial = 0;
+        double value1;
+        Double value2 = 0.0;
+        Double value3 = 0.0;
+
+        System.out.println("Current Value"+"\n" + initial);                    //For show, does nothing com
+        //System.out.println("Enter exit to close calculator");
+        value1 = Console.getDoubleInput("Enter value");       //Input value1
+
+        for (int i = 0; i<=100; i++) {
+            operate[op] = Console.getStringInput("Choose an operation. Enter 'clear' to clear");
+            if (operate[op].equals("clear")){
+                value1 = 0.0;
+                value2 = 0.0;
+                value3 = 0.0;
+                System.out.println("Display Value"+"\n" + value1);
+                value1 = Console.getDoubleInput("Enter value") ;
+            } else if (operate[op].equals("exit")) {
+                break;
+
+            } else if (operate[op].equals("+") || operate[op].equals("-") || operate[op].equals("*") || operate[op].equals("/")){
+                value2 = Console.getDoubleInput("Enter value 2.");    //Input value2
+                value3 = Console.basicmath(operate[op], value1, value2);
+                value1 = value3;                                                    //set value 1 is 3
+                System.out.println("Display Value is " + value1);
+
+            } else if (operate[op].equals("trig")) {
+                value3 = Console.trigFunctions(value1);
+                System.out.println("Display Value is " + value3);
+                value1 = value3;
+            } else if(operate[op].equals("exponent")) {
+                value3 = Console.exponents(value1);
+                System.out.println("Display Value is " + value3);
+                value1 = value3;
+            } else {
+                System.out.println("Error");
+                value1 = Console.getDoubleInput("Enter a value");
+            }
+
+            op++;                                                                    //Keep track outside loop
+        }
+
+        //Console.println("The user input %s as a string", s);        //not needed, just warmup
+        //Console.println("The user input %s as a integer", val1);       //not needed, just warmup
+        //Console.println("The user input %s as a d", val2);             //not needed, just warmup
+        //Console.println("The output is %f", value3);
+        //public static Double basicmath(args) this didn't work
+        System.out.println("Shutting down");
+
+
+
     }
+
+
 }
